2abb7f6aa8e7c36c612165ffbfc15167ed4388da
System Manager <manager@bl8vbox.unassigned-domain>
2018-12-11 12:02:52 +0000

    add answers for group task 2

--- group_task_2/downsample_raw_data.sh
+++
@@ -1,9 +0,0 @@
- #!/bin/bash
-
-while read -r sample url; do 
-  echo "Extracting 2 million reads from $sample..."
-  zless $sample"_raw.fq.gz" | sed -n 1,8000000p > $sample".fq"
-  echo "Compressing $sample subset..."
-  gzip $sample".fq"
-done < samples.txt
-echo "Done"
